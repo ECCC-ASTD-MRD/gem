217b9fbdbd4c6e20db80de530226f61f66a731cb
Stéphane Gaudreault <stephane.gaudreault2@canada.ca>
2019-09-10 15:12:53 -0400

    Fix from Maria for out of bounds error in surface code

--- src/rpnphy/surface/svs_configs.F90
+++ src/rpnphy/surface/svs_configs.F90
@@ -16,6 +16,8 @@ module svs_configs
   implicit none
 
   ! INDEXES FOR SVS SURFACES
+  integer, parameter :: svs_tilesp1  = 5 ! SVS tile + 1 for aggregated value
+  !
   integer, parameter :: indx_svs_ag  = 1 ! AGGREGATED VALUE
   integer, parameter :: indx_svs_bg  = 2 ! BARE GROUND
   integer, parameter :: indx_svs_vg  = 3 ! VEGETATION (NOT COVERED BY SNOW)
@@ -326,7 +328,7 @@ contains
 
     integer I, NI
     real, dimension(ni) :: FVH,FVL,FSNVH,FSN
-    real, dimension(ni,indx_svs_ag) :: WT
+    real, dimension(ni,svs_tilesp1) :: WT
     real, parameter ::  min_wt=0.001 ! [1.=100%] minimum value of weight, otherwise set to zero.
 
     do I=1,NI
