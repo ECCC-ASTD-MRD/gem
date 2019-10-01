7e6bc819d6bc800dabb5418dd1ec25955c88e123
Stéphane Gaudreault <stephane.gaudreault2@canada.ca>
2019-10-01 10:12:23 -0400

    expo should always use height coordinates

--- src/gemdyn/base/gemdm_config.F90
+++ src/gemdyn/base/gemdm_config.F90
@@ -165,12 +165,8 @@
             Dynamics_hydro_L = .false.
             call fislh_hybrid ( hyb_H, G_nk)
          case ('DYNAMICS_EXPO_H')
-            if (Schm_autobar_L) then
-               ! TODO : temporary
-               call set_zeta ( hyb, G_nk )
-            else
-               stop 'EXPO gemdm_config : Not yet implemented'
-            end if
+            Dynamics_hydro_L = .false.
+            call exp_hybrid ( hyb_H, G_nk )
       end select
 
       Schm_nith = G_nk
