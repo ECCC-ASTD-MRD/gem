e24a647c8365d38369343703ddc446e9474ff09d
Samuel Gilbert <samuel.gilbert@canada.ca>
2019-10-09 15:50:54 -0400

    Better OS version detection + fix cpu arch

--- project/os.sh
+++ project/os.sh
@@ -1,47 +1,40 @@
 #!/bin/sh
 
 OSNAME=
-OSVERSION=
+OSVERSION=unknown
 OS=
 
 # remove white spaces
-sansespace() { echo $* | sed 's/ //g'; }
+delSpaces() { echo $* | sed 's/ //g'; }
 
 EC_BASE_ARCH=`echo $BASE_ARCH`
 
 # use local variable at EC
-if [ -n "${EC_BASE_ARCH}" ];
-then
+if [ -n "${EC_BASE_ARCH}" ]; then
     OS="${EC_BASE_ARCH}";
 
-elif [ -f /etc/os-release ];
-then
+elif [ -f /etc/os-release ]; then
     . /etc/os-release
-    #remove white spaces
-    #OSNAME=${NAME// /}
-    OSNAME=`sansespace ${NAME}`
-    if [ -n "${VERSION_ID}" ];
-    then
+    OSNAME=`delSpaces ${NAME}`
+    if [ -n "${VERSION_ID}" ]; then
         OSVERSION="${VERSION_ID}"
+    elif [ -n "${BUILD_ID}" ]; then
+        OSVERSION="${BUILD_ID}"
     fi
-    OS="${OSNAME}-${OSVERSION}-`uname -p`";
+    OS="${OSNAME}-${OSVERSION}-`uname -m`";
 
-elif [ -x /usr/bin/lsb_release ];
-    then
-        OSNAME=`/usr/bin/lsb_release -is`;
-        OSVERSION=`/usr/bin/lsb_release -rs`;
-        OS="${OSNAME}-${OSVERSION}-`uname -p`";
+elif [ -x /usr/bin/lsb_release ]; then
+    OSNAME=`/usr/bin/lsb_release -is`;
+    OSVERSION=`/usr/bin/lsb_release -rs`;
+    OS="${OSNAME}-${OSVERSION}-`uname -m`";
 
-elif [ `uname` = "Darwin" ];
-    then
-        OSNAME="Darwin";
-        OSVERSION=`uname -r`;
-        OS="${OSNAME}-${OSVERSION}";
+elif [ `uname` = "Darwin" ]; then
+    OSNAME="Darwin";
+    OSVERSION=`uname -r`;
+    OS="${OSNAME}-${OSVERSION}";
+
 else 
     OS="Unknown-OS";
 fi
-
-#OSNAME="${OSNAME%\"}"
-#OSNAME="${OSNAME#\"}"
 
 printf "${OS}"
