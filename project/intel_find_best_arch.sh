a44c8e46f6f28d6a0b165beb05c89acca6116fe0
Samuel Gilbert <samuel.gilbert@canada.ca>
2020-03-16 18:41:02 -0400

    Find the best arch automatically to avoid illegal instructions

---
+++ project/intel_find_best_arch.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Since the Intel compilers do not support "-march=native", we try to fin the most
+# performant architecture
+
+for arch in avx512 avx2 avx; do
+    grep flags /proc/cpuinfo | head -n 1 | grep -q $arch && break
+done
+
+if [[ "$arch" = "avx512" ]]; then
+    echo -n "skylake-avx512"
+elif [[ "$arch" = "avx2" ]]; then
+    echo -n "core-avx2"
+elif [[ "$arch" = "avx" ]]; then
+    echo -n "core-avx-i"
+else
+    # Bail out at core2
+   echo -n "core2"
+fi
