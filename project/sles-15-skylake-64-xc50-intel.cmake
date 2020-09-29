540fb19f42ddd0e54d2f372a19a5e5b03fcfdd34
Chantal Pic <chantal.pic2@canada.ca>
2020-09-29 19:22:48 -0400

    Charger un fichier cmake selon la valeur de ORDENV_PLAT. Changements mineurs dans le setup initial.

--- project/sles-15-skylake-64-xc50-intel.cmake
+++ project/sles-15-skylake-64-xc50-intel.cmake
@@ -1,7 +1,5 @@
 add_definitions(-DLittle_Endian -DWITH_intel -DAMD64 -DLINUX_X86_64 -DECCCGEM)
 
-#add_definitions(-DLittle_Endian)
-
 find_program(CMAKE_C_COMPILER "icc")
 find_program(CMAKE_Fortran_COMPILER "ifort")
 
@@ -24,23 +22,3 @@ set(CMAKE_Fortran_FLAGS "-traceback -assume byterecl -convert big_endian -xCORE-
 
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,--allow-shlib-undefined -fpic -Bstatic -static-libgcc -static")
 
-# The Intel compilers do not support -march=native like GCC does
-# This will have to be edited in order to get the most out of the hardware
-
-# If you run into unknown instruction errors at runtime, you will have to change the
-# target architecture specified for "-march".
-
-# Here are architecture names given in descending order of performance:
-#     skylake-avx512
-#     skylake
-#     core-avx2
-#     core-avx-i
-#     corei7-avx
-#     core2
-
-#Michel: les lignes suivantes remplaceraient '-xCORE-AVX512' with '-march=skylake-avx512'
-#execute_process(COMMAND sh "-c" "${CMAKE_CURRENT_SOURCE_DIR}/intel_find_best_arch.sh" OUTPUT_VARIABLE best_arch ERROR_QUIET)
-#message(STATUS "Most performant -march for the Intel compilers on this hardware: ${best_arch}")
-
-#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=${best_arch}")
-#set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -march=${best_arch}")
