989288cf4782bc59e894d8a4f2e08fddb5c2cc3d
Chantal Pic <chantal.pic2@canada.ca>
2020-11-23 18:17:15 -0500

    Ménage dans CMakeLists.txt et fichiers cmake. Mise à jour numéro de version.

--- project/sles-15-skylake-64-xc50-intel.cmake
+++ project/sles-15-skylake-64-xc50-intel.cmake
@@ -4,17 +4,6 @@ find_program(CMAKE_Fortran_COMPILER "ifort")
 find_program(MPI_C_COMPILER "cc")
 find_program(MPI_Fortran_COMPILER "ftn")
 
-# I don't know why, but enable_language empties CMAKE_BUILD_TYPE!
-# We therefore have to back it up and restore it after enable_language
-set(TMP_BUILD_TYPE ${CMAKE_BUILD_TYPE})
-
-# Enable the two languages that are used
-enable_language(C)
-enable_language(Fortran)
-
-# Reset CMAKE_BUILD_TYPE
-set(CMAKE_BUILD_TYPE ${TMP_BUILD_TYPE})
-
 add_definitions(-DLittle_Endian -DECCCGEM)
 
 set(EXTRA_LIBRARIES "-liomp5 -L/opt/gcc/default/snos/lib64 -lcilkrts")
@@ -23,12 +12,12 @@ set(AVG_FLAG -O2)
 set(FAST_FLAG -O2)
 #set(FAST_FLAG -O3 -fast-transcendentals -no-prec-div -ip -no-prec-sqrt)
 
-set(CMAKE_C_FLAGS_DEBUG "-g")
-set(CMAKE_C_FLAGS_RELEASE "-O2")
+set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -g")
+set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O2")
 set(CMAKE_C_FLAGS "-traceback -fpic -xCORE-AVX512 -fp-model precise" CACHE STRING "C compiler flags" FORCE)
 
-set(CMAKE_Fortran_FLAGS_DEBUG "-g")
-set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
+set(CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS_DEBUG} -g -ftrapuv")
+set(CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS_RELEASE} -O2")
 set(CMAKE_Fortran_FLAGS "-traceback -assume byterecl -convert big_endian -xCORE-AVX512 -align array32byte -fpe0 -fpic -ip  -diag-disable=cpu-dispatch -static-intel -diag-disable 7713 -diag-disable 10212 -diag-disable 5140  -fp-model source -I." CACHE STRING "Fortran compiler flags" FORCE)
 
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,--allow-shlib-undefined,--allow-multiple-definition -fpic -Bstatic -static-libgcc -static")
--- project/ubuntu-18.04-skylake-64-intel.cmake
+++ project/ubuntu-18.04-skylake-64-intel.cmake
@@ -2,28 +2,14 @@
 find_program(CMAKE_C_COMPILER icc)
 find_program(CMAKE_Fortran_COMPILER ifort)
 
-# I don't know why, but enable_language empties CMAKE_BUILD_TYPE!
-# We therefore have to back it up and restore it after enable_language
-set(TMP_BUILD_TYPE ${CMAKE_BUILD_TYPE})
-
-# Enable the two languages that are used
-enable_language(C)
-enable_language(Fortran)
-
-# Reset CMAKE_BUILD_TYPE
-set(CMAKE_BUILD_TYPE ${TMP_BUILD_TYPE})
-
-# find_package() commands can only be called after the languages have been 
-# enabled or they will fail
-
 add_definitions(-DLittle_Endian)
 
-set(CMAKE_C_FLAGS_DEBUG "-g")
-set(CMAKE_C_FLAGS_RELEASE "-O2")
+set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -g")
+set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O2")
 set(CMAKE_C_FLAGS "-mkl -Wl,--allow-shlib-undefined -Wtrigraphs -xCORE-AVX512 -fpic -traceback -fp-model precise" CACHE STRING "C compiler flags" FORCE)
 
-set(CMAKE_Fortran_FLAGS_DEBUG "-g")
-set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
+set(CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS_DEBUG} -g -ftrapuv")
+set(CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS_RELEASE} -O2")
 set(CMAKE_Fortran_FLAGS "-mkl -assume byterecl -convert big_endian -xCORE-AVX512 -align array32byte -fpe0 -fpic -traceback -ip -diag-disable=cpu-dispatch -static-intel -diag-disable 7713 -diag-disable 10212 -diag-disable 5140 -fp-model source" CACHE STRING "Fortran compiler flags" FORCE)
 
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,--allow-shlib-undefined,--allow-multiple-definition -fpic -static-intel -mkl")
