aefda7d0d7ef7bc863ac206e7de6a24145741240
Chantal Pic <chantal.pic2@canada.ca>
2020-12-08 16:07:35 -0500

    Passage à la version 5.1.0-rc3.

--- src/maingem/maincheckdmpart.F90
+++ src/maingem/maincheckdmpart.F90
@@ -7,8 +7,8 @@ subroutine atm_model_getversion2(name_S,version_S,date_S,arch_S,compil_S,user_S,
    character(len=*),intent(out) :: name_S,version_S,date_S,arch_S,user_S,compil_S
    logical,intent(out) :: is_official_L
    name_S = "checkdmpart"
-   version_S = "5.1.0-rc2"
-   date_S = "2020-11-19 00:07 GMT"
+   version_S = "5.1.0-rc3"
+   date_S = "2020-12-08 14:13 GMT"
    arch_S = ""
    user_S = ""
    compil_S = ""
--- src/maingem/maingemdm.F90
+++ src/maingem/maingemdm.F90
@@ -7,8 +7,8 @@ end program gem
           character(len=*),intent(out) :: name_S,version_S,date_S,arch_S,user_S,compil_S
           logical,intent(out) :: is_official_L
           name_S = "GEMDM"
-          version_S = "5.1.0-rc2"                                                                                                                                                                                                                   
-          date_S = "2020-11-19 00:07 GMT"
+          version_S = "5.1.0-rc3"
+          date_S = "2020-12-08 14:13 GMT"
           arch_S = ""
           user_S = ""
           compil_S = ""
