75333f5ed9f37705354cb6460fdd574d1314e37b
Chantal Pic <chantal.pic2@canada.ca>
2021-07-20 17:58:49 -0400

    Clean-up.

--- project/ubuntu-18.04-skylake-64-intel.cmake
+++ project/ubuntu-18.04-skylake-64-intel.cmake
@@ -18,15 +18,14 @@ set(CMAKE_BUILD_TYPE ${TMP_BUILD_TYPE})
 
 add_definitions(-DLittle_Endian)
 
-set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -g")
-set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O2")
-set(CMAKE_C_FLAGS "-mkl -Wl,--allow-shlib-undefined -Wtrigraphs -xCORE-AVX512 -fpic -traceback -fp-model precise" CACHE STRING "C compiler flags" FORCE)
+set(CMAKE_C_FLAGS_DEBUG "-g")
+set(CMAKE_C_FLAGS_RELEASE "-O2")
+set(CMAKE_C_FLAGS "-mkl -Wtrigraphs -xCORE-AVX512 -fpic -traceback -fp-model precise" CACHE STRING "C compiler flags" FORCE)
 
-set(CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS_DEBUG} -g -ftrapuv")
-set(CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS_RELEASE} -O2")
+set(CMAKE_Fortran_FLAGS_DEBUG "-g -ftrapuv")
+set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
 set(CMAKE_Fortran_FLAGS "-mkl -assume byterecl -convert big_endian -xCORE-AVX512 -align array32byte -fpe0 -fpic -traceback -ip -diag-disable=cpu-dispatch -static-intel -diag-disable 7713 -diag-disable 10212 -diag-disable 5140 -fp-model source" CACHE STRING "Fortran compiler flags" FORCE)
 
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,--allow-shlib-undefined,--allow-multiple-definition -fpic -static-intel -mkl")
-#set(CMAKE_EXE_LINKER_FLAGS_INIT "-fpic -static-intel -mkl")
 
 set(EXTRA_LIBRARIES "-lopen-pal")
