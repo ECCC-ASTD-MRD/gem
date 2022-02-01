9cfb54eb0ce0182ccd29dc3d4343c8c2ce852089
Chantal Pic <chantal.pic2@canada.ca>
2022-02-01 17:28:00 -0500

    Update to version 5.1.1.

--- src/maingem/maingemdm.F90
+++ src/maingem/maingemdm.F90
@@ -2,16 +2,16 @@ program gem
    call gemdm
 end program gem
 
-       subroutine atm_model_getversion2(name_S,version_S,date_S,arch_S,compil_S,user_S,is_official_L)
-          implicit none
-          character(len=*),intent(out) :: name_S,version_S,date_S,arch_S,user_S,compil_S
-          logical,intent(out) :: is_official_L
-          name_S = "GEMDM"
-          version_S = "5.1.0"
-          date_S = "2021-10-01 16:45 GMT"
-          arch_S = ""
-          user_S = ""
-          compil_S = ""
-          is_official_L = .false.
-          return
-       end subroutine atm_model_getversion2
+subroutine atm_model_getversion2(name_S,version_S,date_S,arch_S,compil_S,user_S,is_official_L)
+  implicit none
+  character(len=*),intent(out) :: name_S,version_S,date_S,arch_S,user_S,compil_S
+  logical,intent(out) :: is_official_L
+  name_S = "GEMDM"
+  version_S = "5.1.1"
+  date_S = "2022-01-27 14:49 GMT"
+  arch_S = ""
+  user_S = ""
+  compil_S = ""
+  is_official_L = .false.
+  return
+end subroutine atm_model_getversion2
