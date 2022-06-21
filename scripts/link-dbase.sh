641a9ff2ac23969c557438e4147674e180bd407f
Chantal Pic <chantal.pic2@canada.ca>
2022-06-21 18:19:08 -0400

    Load rmn libs and fix link to database on U2. Update submodules.

--- scripts/link-dbase.sh
+++ scripts/link-dbase.sh
@@ -2,10 +2,12 @@
 
 # link to database at CMC
 
-if [[ "$(hostname -A)" =~ cmc.ec.gc.ca ]]; then
+DOMAIN=`hostname -d`
+
+if [[ "${DOMAIN}"  = cmc.ec.gc.ca ]]; then
     GEM_DBASE=/home/ordenv/ssm-domains9/release/gem-data_4.2.0/gem-data_4.2.0_all/share/data/dfiles
     [ -e ${GEM_DBASE} ] && [ ! -e gem_dbase ] && ln -sf ${GEM_DBASE} gem_dbase
-elif [[ "$(hostname -A)" =~ science.gc.ca ]]; then
+elif [[ -z "${DOMAIN}" || ${DOMAIN} = "science.gc.ca" ]]; then
     GEM_DBASE=/fs/ssm/eccc/mrd/rpn/MIG/GEM/d/gem-data/gem-data_4.2.0/gem-data_4.2.0_all/share/data/dfiles
     [ -e ${GEM_DBASE} ] && [ ! -e gem_dbase ] && ln -sf ${GEM_DBASE} gem_dbase
 else
