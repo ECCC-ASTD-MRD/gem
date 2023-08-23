7ea6aa70f9f337805f3ba5da731b484215d9f501
Chantal Pic <chantal.pic@ec.gc.ca>
2023-08-23 14:56:17 -0400

    Update RPN-SI libs and programs

--- src/rpn-si/filetype/filetyp.f90
+++ src/rpn-si/filetype/filetyp.f90
@@ -3,7 +3,7 @@
       implicit none
       character *8 cle(10)
       character *4096 def(10),val(10)
-      character *60 msgs(-1:37)
+      character *60 msgs(-1:39)
       integer wkoffit,ipos,ier,t(8),i
 
       data cle /'L ',8*'T','-.'/
@@ -49,6 +49,8 @@
       data msgs(35) /'FICHIER NETCDF'/
       data msgs(36) /'FICHIER CMCARC v4'/
       data msgs(37) /'FICHIER CMCARC v5'/
+      data msgs(38) /'FICHIER HDF5'/
+      data msgs(39) /'RPN standard 98 with RSF backend'/
 
       ipos = 0
       call ccard (cle,def,val,10,ipos)
