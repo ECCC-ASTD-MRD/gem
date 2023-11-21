ac2fbc130aa11b8d354e9386dd23ee860b48ff3b
Chantal Pic <chantal.pic@ec.gc.ca>
2023-11-21 11:49:20 -0500

    Removing gemdyn, as history has changed

--- src/gemdyn/src/base/fftw3.F90
+++
@@ -1,9 +0,0 @@
-! FFTW3 module, as per directions in section 7.7 of the FFTW3 manual.
-
-! Installing FFTW as a module helps segregate these routines from the
-! remainder of the GEM code
-
-module FFTW3
-   use, intrinsic :: iso_c_binding
-#include "fftw3.f03"
-end module
