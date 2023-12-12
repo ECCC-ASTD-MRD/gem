e44565f2199bc10fee05f41966d0387deb3cab51
Chantal Pic <chantal.pic@ec.gc.ca>
2023-12-12 16:34:26 -0500

    Minor

--- scripts/link-dbase.sh
+++ scripts/link-dbase.sh
@@ -4,8 +4,13 @@
 
 GEM_DBASE=/fs/ssm/eccc/mrd/rpn/MIG/GEM/d/gem-data/gem-data_4.2.0/gem-data_4.2.0_all/share/data/dfiles
 
-if [ -e ${GEM_DBASE} ] &&  [ ! -e gem_dbase ] ; then
-    ln -sf ${GEM_DBASE} gem_dbase
+if [ -e ${GEM_DBASE} ] ; then
+    if  [ ! -e gem_dbase ] ; then
+        ln -sf ${GEM_DBASE} gem_dbase
+    else
+        echo "There is already a link to a database: doing nothing."
+        ls -al gem_dbase
+    fi
 else
     echo "Database not found: don't know where database is."
 fi
