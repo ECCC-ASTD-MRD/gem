55a6130af4d60a0c70b4f8384e292df7545bcee1
Chantal Pic <chantal.pic@ec.gc.ca>
2024-01-30 16:12:56 -0500

    Better check for GEM database

--- scripts/link-dbase.sh
+++ scripts/link-dbase.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# link to database at CMC
-
+# link to GEM database at CMC
 GEM_DBASE=/fs/ssm/eccc/mrd/rpn/MIG/GEM/d/gem-data/gem-data_4.2.0/gem-data_4.2.0_all/share/data/dfiles
 
-if [ -e ${GEM_DBASE} ] ; then
-    if  [ ! -e gem_dbase ] ; then
-        ln -sf ${GEM_DBASE} gem_dbase
-    else
-        echo "There is already a link to a database: doing nothing."
-        ls -al gem_dbase
-    fi
-else
-    echo "Database not found: don't know where database is."
+if [ ! -d ${GEM_DBASE} ] ; then
+    echo "${GEM_DBASE} not found: don't know where database is."
+    exit 1
 fi
+
+# remove possible existing link or file, and create symbolic link, 
+# or display an error message (for example if a directory named gem_dbase already exists)
+\rm -f gem_dbase && ln -s ${GEM_DBASE} gem_dbase || ( echo "gem_dbase cannot be removed" && exit 2)
