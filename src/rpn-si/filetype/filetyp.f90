d82ea3804d302754df0621fd4da16d18b958e81b
Chantal Pic <chantal.pic@ec.gc.ca>
2024-01-30 15:50:59 -0500

    Update RPN-SI libraries

--- src/rpn-si/filetype/filetyp.f90
+++ src/rpn-si/filetype/filetyp.f90
@@ -3,7 +3,7 @@
       implicit none
       character *8 cle(10)
       character *4096 def(10),val(10)
-      character *60 msgs(-1:39)
+      character *60 msgs(-1:41)
       integer wkoffit,ipos,ier,t(8),i
 
       data cle /'L ',8*'T','-.'/
@@ -51,6 +51,8 @@
       data msgs(37) /'FICHIER CMCARC v5'/
       data msgs(38) /'FICHIER HDF5'/
       data msgs(39) /'RPN standard 98 with RSF backend'/
+      data msgs(40) /'Generic RSF'/
+      data msgs(41) /'SQLite 3 database'/
 
       ipos = 0
       call ccard (cle,def,val,10,ipos)
