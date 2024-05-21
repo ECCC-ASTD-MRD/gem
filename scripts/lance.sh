50887bf8595ece2fdc84960a8a78d39db22089e8
Chantal Pic <chantal.pic@ec.gc.ca>
2024-05-21 10:14:56 -0400

    Squashed 'share/gem-maestro/' changes from 45d73939..b97e7da8
    51391977 gem-maestro: Spectral nudging update
    
    git-subtree-dir: share/gem-maestro
    git-subtree-split: b97e7da89641b9617758d7dd87c83c7025595aca

--- scripts/lance.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -x
-ulimit -S -s unlimited
-$1
--- scripts/make_work_post.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-#
-
-if [ -n "${GEM_STORAGE_DIR}" -a -n "${GEM_ARCH}" -a  -n "${GEM_WORK}" ] ; then
-   cd ${GEM_WORK}
-   /bin/rm -f *.sh *.ksh *.py *.dot grille findtopo r.* vstudio
-fi
