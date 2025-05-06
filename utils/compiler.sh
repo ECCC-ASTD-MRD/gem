e01984865fa6fd4bbad5208f05bf7ff2e82a41e6
Chantal Pic <chantal.pic@ec.gc.ca>
2025-05-06 12:02:21 -0400

    Bettter check on ifx and ifort
    Avoid a superfluous warning ifort is not found.
    For backwards compatibility with older versions of Intel, since ifx has been around since 2021.

--- utils/compiler.sh
+++ utils/compiler.sh
@@ -10,8 +10,21 @@ case $1 in
         COMPILER_VERSION=$(gfortran --version | head -n 1 | sed "s/.*) \([^ ]*\).*$/\1/")
         ;;
     intel)
-        type ifort >/dev/null &&
-            COMPILER_VERSION=`ifort -V 2>&1 | head -n 1 | sed "s/.*Version \([^ ]*\).*$/\1/"`
+        # 1. We have to redirect to &1 as ifx -V and ifort -V output is done on stderr!
+        # 2. We don't want an error message if ifx is not found, while ifort is found
+        #    So, first, check for ifx and if not found, then check for ifort.
+        _VERSIONx=$(ifx -V 2>&1)
+        if [ $? == 0 ]; then # on a trouvé ifx
+            COMPILER_VERSION=$(echo ${_VERSIONx} | head -n 1 | sed "s/.*Version \([^ ]*\).*$/\1/")
+        else
+            _VERSIONo=$(ifort -V 2>&1)
+            if [ $? == 0 ]; then # on a trouvé ifort
+                COMPILER_VERSION=$(echo ${_VERSIONo} | head -n 1 | sed "s/.*Version \([^ ]*\).*$/\1/")
+            else  # on n'a ni ifx, ni ifort
+                ( echo ${_VERSIONx}; echo ${_VERSIONo} ) >&2
+            fi
+        fi
+        ;;
     llvm)
         COMPILER_VERSION=$(clang --version | grep version | sed "s/.*version \([^ ]*\).*$/\1/")
         ;;
