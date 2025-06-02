48d3ec286b3e1ddecbfd727622b3d050e1e9d6b1
Vivian Lee <vivian.lee@ec.gc.ca>
2025-06-02 16:56:10 -0400

    Test also for RSF files

--- scripts/prep_domain.sh
+++ scripts/prep_domain.sh
@@ -106,7 +106,7 @@ if [ -e "${anal}" ] ; then
      set +x
    else
       is_cmcarc=${work}/.is_cmcarc ; rm -f ${is_cmcarc}
-      if r.filetype ${analysis} -t 36 37 ; then touch ${is_cmcarc} ; fi
+      if r.filetype ${analysis} -t 36 37 39; then touch ${is_cmcarc} ; fi
       if [[ -e ${is_cmcarc} ]] ; then
         mkdir -p ${work}/unpack_archive
         cd ${work}/unpack_archive
@@ -133,7 +133,7 @@ if [ -e "${anal}" ] ; then
    fi
    cd ${work}
 
-   if r.filetype ${local_anal_file} -t 1 33 ; then
+   if r.filetype ${local_anal_file} -t 1 33 39; then
 
    # Run the user-defined headscript
    final_file=${o}/ANALYSIS
