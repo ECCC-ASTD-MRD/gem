bc4320078a4b771f9168ca40acc7a89dab0db9f7
Jean-Francois Cossette <jean-francois.cossette@ec.gc.ca>
2025-09-08 17:10:16 +0000

    gem: output mem stats to stdout instead of stderr

--- src/gem/maingemdm.F90
+++ src/gem/maingemdm.F90
@@ -12,6 +12,7 @@ program gem
    integer(kind=int32) ierror
 
    app_ptr=app_init(0,PROJECT_NAME_STRING,VERSION,PROJECT_DESCRIPTION_STRING,BUILD_TIMESTAMP)
+   call app_logstream("stdout")
    call app_libregister(APP_LIBVGRID,HAVE_VGRID//c_null_char)
    call app_libregister(APP_LIBTDPACK,HAVE_TDPACK//c_null_char)
    call app_libregister(APP_LIBDYN,dyn_VERSION//c_null_char)
