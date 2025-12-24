a849c0f131a7b1ff9fa02c21388bdea046fcf601
Chantal Pic <chantal.pic@ec.gc.ca>
2025-12-24 11:54:49 -0500

    Update CMakeLists.txt files

--- src/gem/maingemdm.F90
+++ src/gem/maingemdm.F90
@@ -3,16 +3,24 @@ program gem
    use iso_fortran_env
    implicit none
 
+#ifdef HAVE_MACH
+#include <gemmach_build_info.h>
+#else
 #include <gem_build_info.h>
+#endif
 
    integer(kind=int32) ierror
 
    app_ptr=app_init(0,PROJECT_NAME_STRING,VERSION,PROJECT_DESCRIPTION_STRING,BUILD_TIMESTAMP)
+   call app_logstream("stdout")
    call app_libregister(APP_LIBVGRID,HAVE_VGRID//c_null_char)
    call app_libregister(APP_LIBTDPACK,HAVE_TDPACK//c_null_char)
    call app_libregister(APP_LIBDYN,dyn_VERSION//c_null_char)
    call app_libregister(APP_LIBPHY,phy_VERSION//c_null_char)
    call app_libregister(APP_LIBMDLUTIL,modelutils_VERSION//c_null_char)
+#ifdef WITH_MACH
+   call app_libregister(APP_LIBMACH,mach_VERSION//c_null_char)
+#endif
 
    call MPI_INIT(ierror)
    call app_start()
@@ -44,4 +52,5 @@ program gem
 !   call MPI_FINALIZE(ierror)  
 
    app_status=app_end(-1)
+   call rpn_comm_FINALIZE(ierror)
 end program gem
